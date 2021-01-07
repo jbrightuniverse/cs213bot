adea5606845e0b029e56a81b6b623d0f11710c21
James Yu <jbrightuniverse@gmail.com>
2021-01-06 21:21:27 -0800

    Initial commit for sm213 simulator

---
+++ cogs/sm213.py
@@ -0,0 +1,30 @@
+import os
+import random
+from datetime import datetime
+from os.path import isfile, join
+
+import discord
+from discord.ext import commands
+
+from util.badargs import BadArgs
+
+
+class Sm213(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @bot.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def sim(self, ctx):
+        """
+        `!sim` __`Launch SM213 simulator`__
+
+        **Usage:** !sim
+
+        **Examples:** `!sim` launches simulator
+        """
+        await ctx.send("ok")
+
+
+def setup(bot):
+    bot.add_cog(Sm213(bot))
